9276082ece231616af1c11b2affd031dbe3ffb4c
Leandro <leandrops.dev@gmail.com>
2023-05-09 09:51:44 -0300

    cadastro novo usuário

--- novo.php
+++ novo.php
@@ -1,17 +1,20 @@
 <?php 
 
     //Verificar se a postagem existe de acordo com os campos
-    if(isset($_POST["name"]) && isset($_POST["email"]) && isset($_POST["password"])){
-
-        $name = $_POST["name"];
-        $email = $_POST["email"];
-        $password = md5($_POST["password"]);
-        $status = $_POST["status"];
+    if(isset($_POST["name"]) && isset($_POST["email"]) && isset($_POST["password"]) && isset($_POST["confirm-password"])){
 
         //Verificar se os campos foram preenchidos
-        if(empty($name) or empty($email) or empty($password) or empty($status)){
+        if(empty($_POST["name"]) or empty($_POST["email"]) or empty($_POST["password"]) or empty($_POST["confirm-password"]) or empty($_POST["status"])){
             $erro_geral = "Todos os campos são obrigatórios!";
+        }elseif($_POST["password"] != $_POST["confirm-password"]){
+            $erro_geral = "A confirmação da senha está errada!";
+            $senhas_incorretas = "Senhas incorretas!";
         }else{
+            $name = $_POST["name"];
+            $email = $_POST["email"];
+            $password = md5($_POST["password"]);
+            $status = $_POST["status"];
+
             $sql = "INSERT INTO users (name, email, password, status) VALUES ('{$name}', '{$email}', '{$password}', '{$status}')";
             $res = $conn->query($sql);
 
@@ -36,27 +39,31 @@
 
     <?php echo (isset($erro_geral) ? "<div id='usuarios-mensagem'>".$erro_geral."</div>" : ""); ?>
 
-    <div class="usuario-form">
-        <input type="text" id="name" name="name" placeholder="Digite o nome" value="<?php echo ($_POST['name'] ? $_POST['name'] : ""); ?>" />
+    <div class="usuario-form <?php echo (isset($erro_geral) && $_POST['name'] == "" ? "alertRed" : ""); ?>">
+        <label for="name">Nome do usuário</label>
+        <input type="text" id="name" name="name" placeholder="Digite o nome" value="<?php echo (isset($_POST['name']) ? $_POST['name'] : ""); ?>" />
+    </div>
+
+    <div class="usuario-form <?php echo (isset($erro_geral) && $_POST['email'] == "" ? "alertRed" : ""); ?>">
+        <label for="email">E-mail do usuário</label>
+        <input type="email" id="email" name="email" placeholder="Digite o e-mail" value="<?php echo (isset($_POST['email']) ? $_POST['email'] : ""); ?>" />
+    </div>
+
+    <div class="usuario-form <?php echo (isset($erro_geral) && $_POST['password'] == "" ? "alertRed" : ""); ?>">
+        <label for="password">Senha do usuário</label>
+        <input type="password" id="password" minlength="6" name="password" placeholder="Digite sua senha" value="<?php echo (isset($_POST['password']) ? $_POST['password'] : ""); ?>" >
+    </div>
+
+    <div class="usuario-form <?php echo (isset($erro_geral) && $_POST['confirm-password'] == "" ? "alertRed" : ""); ?>">
+        <label for="confirm-password">Confirme a senha</label>
+        <input type="password" id="confirm-password" name="confirm-password" placeholder="Confirme a senha" value="<?php echo (isset($_POST['confirm-password']) ? $_POST['confirm-password'] : ""); ?>" >
     </div>
 
     <div class="usuario-form">
-        <input type="email" id="email" name="email" placeholder="Digite o e-mail" value="<?php echo ($_POST['email'] ? $_POST['email'] : ""); ?>" />
-    </div>
-
-    <div class="usuario-form">
-        <input type="password" id="password" minlength="6" name="password" placeholder="Digite sua senha" value="<?php echo ($_POST['password'] ? $_POST['password'] : ""); ?>" >
-    </div>
-
-    <div class="usuario-form">
-        <input type="password" id="confirm-password" name="confirm-password" placeholder="Confirme a senha" value="<?php echo ($_POST['confirm-password'] ? $_POST['confirm-password'] : ""); ?>" >
-    </div>
-
-    <div class="usuario-form">
-        <select name="status" class="form-control">
-            <option value="" disabled selected>Qual a situação do usuário?</option>
-            <option value="Y">Ativo</option>
-            <option value="N">Inativo</option>
+        <label for="status">Status do usuário</label>
+        <select name="status">
+            <option value="Y" <?php echo (isset($erro_geral) && $_POST['status'] == "Y" ? "selected" : ""); ?>>Ativo</option>
+            <option value="N" <?php echo (isset($erro_geral) && $_POST['status'] == "N" ? "selected" : ""); ?>>Inativo</option>
         </select>
     </div>
 
